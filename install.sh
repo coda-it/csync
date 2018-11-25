dc91e93de282121c004e89caba8e49b0d9e54e3f
Oskar Szura <oskar.szura@gmail.com>
2018-11-25 11:24:01 +0100

    Remove cron installation for now

--- install.sh
+++ install.sh
@@ -5,6 +5,3 @@ cp ./csync.sh /usr/local/bin/
 mv /usr/local/bin/csync.sh /usr/local/bin/csync
 chmod 755 /usr/local/bin/csync
 
-#add cron job
-line="* * * * * csync"
-(crontab -u $USER -l; echo "$line" ) | crontab -u $USER -
